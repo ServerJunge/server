a86260d71e8a1a3752524f105d5a0386f075e79a
ServerJunge <serverjunge@live.de>
2014-05-19 19:28:49 +0200

    Little fix for commas
    deleted last comma

--- app/models/software.rb
+++ app/models/software.rb
@@ -2,7 +2,7 @@ class Software < ActiveRecord::Base
 	 before_save :convert_array_to_string
 
 	 def convert_array_to_string
-     self.softwarename = self.softwarename.join(",")
+     self.softwarename = self.softwarename.reject{ |c| c.empty? }.join(",")
      puts self.softwarename
      return true
    end
