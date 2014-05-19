a324d548c6df1a23aebfe62d5248a8d03c0c6f60
ServerJunge <serverjunge@live.de>
2014-05-19 20:06:17 +0200

    Some changes to form

--- app/helpers/software_helper.rb
+++ app/helpers/software_helper.rb
@@ -1,6 +1,6 @@
 module SoftwareHelper
 
 	def software_options
-		['7zip','SysInternals','Office','PDF-Creator']
+		['7zip','SysInternals','Office','PDF-Creator', 'GoogleChrome', 'Javaruntime', 'PDFCreator']
 	end
 end
