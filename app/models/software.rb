6980497f0ea431209a9df6cca1ee1a641213203d
CloudJunge <serverjunge@live.de>
2014-05-23 12:50:40 +0200

    Tweaks

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,9 +1,13 @@
 class Software < ActiveRecord::Base
+
+  validates :softwarename, presence: true, allow_blank: false
+
 	 before_save :convert_array_to_string
 
 	 def convert_array_to_string
      	self.softwarename = self.softwarename.reject{ |c| c.empty? }.join(",")
      	puts self.softwarename
      return true
-   end
+   	 end
+
 end
